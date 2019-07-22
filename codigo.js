83289feee9f0d956b4180f9d02ceaf937abe5612
MateusKurten <51063432+MateusKurten@users.noreply.github.com>
2019-07-22 04:34:49 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,43 @@
+var bubble;
+var d;
+var popped = new Audio('pop.mp3');
+
+function setup(){
+    createCanvas(800,600);
+    background(255);
+    var x = 50 + Math.random()*700;
+    var y = 50 + Math.random()*500;
+    bubble = new Bubble(x, y);
+    bubble.mostrar();
+}
+
+
+function mousePressed(){
+    bubble.estourar();
+}
+
+// Class Bubble
+
+class Bubble{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    mostrar(){
+        stroke("black");
+        strokeWeight(4);
+        ellipse(this.x, this.y, 50, 50);
+    }
+    
+    estourar(){
+        d = dist(mouseX, mouseY, this.x, this.y)
+        if (d < 25){
+            popped.play();
+            clear();
+            this.x = 50 + Math.random()*700;
+            this.y = 50 + Math.random()*500;
+            this.mostrar();
+    }
+}
+}
